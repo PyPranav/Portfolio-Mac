da376d824e4de3fc0854cee1e4d2ae368f664656
PyPranav <pranavsunil2103@gmail.com>
2024-08-15 17:10:43 +0530

    gennie model

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,16 @@
 "use client"
 import "./home.css";
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { scaleValue } from "../utils/scale";
+import { dockAppList } from "@/utils/dockApps";
 
 const maxAdditionalSize = 5;
 
 function App() {
   const dockRef = useRef<HTMLDivElement>(null);
+  const modelRef = useRef<HTMLDivElement>(null);
+
+  const [openedApp, setOpenedApp] = useState<number>(0)
 
   const handleAppHover = (ev: React.MouseEvent<HTMLLIElement>) => {
     if (!dockRef.current) return;
@@ -32,66 +36,95 @@ function App() {
       `${offsetPixels}px`
     );
   };
+  const getCoords = (appNum:number):[number,number,number,number] => {
+    let appCoord = document.querySelectorAll('.app')
+    let rect = appCoord[appNum-1].getBoundingClientRect();
+    return [rect.top+30,rect.left+30,window.innerHeight - rect.bottom+30,window.innerWidth - rect.right + 30]
+  }
+
+  const CloseApp = (appNum:number)=>{
+    if(appNum===0)
+        return
+    let appCoord = getCoords(appNum)
+    console.log({appCoord})
+    if (modelRef.current){
+      modelRef.current.style.top = `${appCoord[0]}px`;
+      modelRef.current.style.left = `${appCoord[1]}px`;
+      modelRef.current.style.bottom = `${appCoord[2]}px`;
+      modelRef.current.style.right = `${appCoord[3]}px`;
+      modelRef.current.style.opacity = "0"
+      
+      modelRef.current.style.clipPath = 'polygon(0 0, 100% 0, 50% 100%, 50% 100%)';
+
+
+      modelRef.current.style.transition = "all 0.5s ease-in-out";    
+    }
+  }
+
+  const OpenApp = (appNum:number)=>{
+    setOpenedApp(appNum)
+    console.log({'opened': appNum})
+    let appCoord = getCoords(appNum)
+    console.log({appCoord})
+    if (modelRef.current){
+      modelRef.current.style.transition = "all 0s ease-in-out";
+      setTimeout(()=>{
+        if (modelRef.current){
+          modelRef.current.style.top = `${appCoord[0]}px`;
+          modelRef.current.style.left = `${appCoord[1]}px`;
+          modelRef.current.style.bottom = `${appCoord[2]}px`;
+          modelRef.current.style.right = `${appCoord[3]}px`;
+          modelRef.current.style.opacity = "0"
+          modelRef.current.style.clipPath = 'polygon(0 0, 100% 0, 50% 100%, 50% 100%)';
+        }
+      },10)
+      setTimeout(()=>{
+        if (modelRef.current)
+          modelRef.current.style.transition = "all 0.5s ease-in-out";
+
+      },15)
+
+      setTimeout(() => {
+        if(modelRef.current){
+          modelRef.current.style.top = `${0}px`; // Adjust these values to control the final size
+          modelRef.current.style.left = `${0}px`;
+          modelRef.current.style.bottom = `${0}px`;
+          modelRef.current.style.right = `${0}px`;
+          modelRef.current.style.opacity = "1";
+          modelRef.current.style.clipPath = 'polygon(0 0, 100% 0, 100% 100%, 0 100%)';
+        }
+      }, 20);
+    }
+  }
 
   return (
     <div className="page">
+      <div
+      ref={modelRef}
+      style={{
+        position:'absolute',
+        // width:0,
+        // height:0,
+        background:'black'
+      }}
+      ></div>
       <nav ref={dockRef} className="dock">
         <ul>
-          <li className="app" onMouseMove={handleAppHover}>
-            <div onClick={()=>{
-              console.log("Finder")
-            }}>
-              <img src="/dock_icons/finder.webp" />
-              <span className="tooltip">Finder</span>
-            </div>
-          </li>
-          <li className="app" onMouseMove={handleAppHover}>
-            <div onClick={()=>{}}>
-              <img src="/dock_icons/chatgpt.webp" />
-              <span className="tooltip">Personal GPT</span>
-            </div>
-          </li>
-          <li className="app" onMouseMove={handleAppHover}>
-            <div onClick={()=>{}}>
-              <img src="/dock_icons/arc.webp" />
-              <span className="tooltip">Social Media</span>
-            </div>
-          </li>
-          
-          
-          
-          <li className="app" onMouseMove={handleAppHover}>
-            <div onClick={()=>{}}>
-              <img src="/dock_icons/settings.webp" />
-              <span className="tooltip">About</span>
-            </div>
-          </li>
-          <li className="app" onMouseMove={handleAppHover}>
-            <div onClick={()=>{}}>
-              <img src="/dock_icons/photos.webp" />
-              <span className="tooltip">Photos</span>
-            </div>
-          </li>
-          
-          
-          <li className="app" onMouseMove={handleAppHover}>
-            <div onClick={()=>{}}>
-              <img src="/dock_icons/spotify.webp" />
-              <span className="tooltip">Spotify</span>
-            </div>
-          </li>
-          <li className="app" onMouseMove={handleAppHover}>
-            <div onClick={()=>{}}>
-              <img src="https://www.frontend.fyi/playground-assets/macos-dock/icons/vscode.png" />
-              <span className="tooltip">Some Game</span>
-            </div>
-          </li>
-          <li className="app" onMouseMove={handleAppHover}>
-            <div onClick={()=>{}}>
-              <img src="/dock_icons/mail.webp" />
-              <span className="tooltip">Contact Me</span>
-            </div>
-          </li>
+          {dockAppList.map((dockApp)=>(
+            <li key={dockApp.id} className="app" onMouseMove={handleAppHover}>
+              <div onClick={()=>{
+                CloseApp(openedApp)
+                if (openedApp!=0)
+                  setTimeout(()=>{
+                    OpenApp(dockApp.id)
+                  },500)
+                else OpenApp(dockApp.id)
+              }}>
+                <img src={dockApp.imageSrc} />
+                <span className="tooltip">{dockApp.tooltip}</span>
+              </div>
+            </li>
+          ))}
         </ul>
       </nav>
 
---
+++ utils/dockApps.ts
@@ -0,0 +1,43 @@
+export const dockAppList = [
+    {
+      "id": 1,
+      "imageSrc": "/dock_icons/finder.webp",
+      "tooltip": "Finder"
+    },
+    {
+      "id": 2,
+      "imageSrc": "/dock_icons/chatgpt.webp",
+      "tooltip": "Personal GPT"
+    },
+    {
+      "id": 3,
+      "imageSrc": "/dock_icons/arc.webp",
+      "tooltip": "Social Media"
+    },
+    {
+      "id": 4,
+      "imageSrc": "/dock_icons/settings.webp",
+      "tooltip": "About"
+    },
+    {
+      "id": 5,
+      "imageSrc": "/dock_icons/photos.webp",
+      "tooltip": "Photos"
+    },
+    {
+      "id": 6,
+      "imageSrc": "/dock_icons/spotify.webp",
+      "tooltip": "Spotify"
+    },
+    {
+      "id": 7,
+      "imageSrc": "https://www.frontend.fyi/playground-assets/macos-dock/icons/vscode.png",
+      "tooltip": "Some Game"
+    },
+    {
+      "id": 8,
+      "imageSrc": "/dock_icons/mail.webp",
+      "tooltip": "Contact Me"
+    }
+  ]
+  
