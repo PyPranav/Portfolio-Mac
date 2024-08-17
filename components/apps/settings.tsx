23d94ae81d2c01e17794e1dfd8a08645b734f2eb
PyPranav <pranavsunil2103@gmail.com>
2024-08-17 14:28:12 +0530

    scroll  fix settings

--- components/apps/settings.tsx
+++ components/apps/settings.tsx
@@ -31,19 +31,21 @@ const SettingsPage = ({CloseApp,openedApp,appStates,setAppStates}:{CloseApp:any,
     return ( 
     
     <Tabs value={appStates[openedApp]['tabValue']} onValueChange={(val)=>setAppStates({...appStates,[openedApp]:{...appStates[openedApp],'tabValue':val}})} defaultValue="account" className="grid grid-cols-[300px_2px_1fr] h-full w-full bg-[#2d2d2d] color-white">
-        <TabsList className="flex flex-col h-full p-3 items-start justify-start bg-[#292929]">
-          <WindowCloseButtons CloseApp={CloseApp} openedApp={openedApp} />
+        <TabsList className="flex flex-col h-full p-3 pt-0 items-start justify-start bg-[#292929] overflow-scroll">
+          <div className="pt-3 w-full sticky top-0 bg-[#292929] z-[1000]">
+            <WindowCloseButtons CloseApp={CloseApp} openedApp={openedApp} />
 
-          <div className="relative  w-full max-w-md py-3">
-            <Input
-              type="search"
-              disabled
-              placeholder="Search"
-              className="w-full rounded-lg border border-[#383637] bg-[#383637] px-4 pl-10 py-2 text-gray-900 focus:border-gray-500 focus:outline-none dark:border-gray-600 dark:bg-gray-800 dark:text-gray-100"
-            />
-            <div className="absolute  inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-              <SearchIcon className="w-5 h-5 text-gray-400" />
-            </div>
+            <div className="relative  w-full max-w-md py-3">
+              <Input
+                type="search"
+                disabled
+                placeholder="Search"
+                className="w-full rounded-lg border border-[#383637] bg-[#383637] px-4 pl-10 py-2 text-gray-900 focus:border-gray-500 focus:outline-none dark:border-gray-600 dark:bg-gray-800 dark:text-gray-100"
+              />
+              <div className="absolute  inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                <SearchIcon className="w-5 h-5 text-gray-400" />
+              </div>
+          </div>
           </div>
 
           <TabsTrigger className="w-full justify-start data-[state=active]:bg-[#195cc5] text-white data-[state=active]:text-white" value="id">
