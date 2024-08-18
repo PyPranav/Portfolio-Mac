8be5e8a67014403d64e7125779550bb0b93d0cc5
PyPranav <pranavsunil2103@gmail.com>
2024-08-18 17:19:55 +0530

    tooltip box fix

--- utils/photos.ts
+++ utils/photos.ts
@@ -8,14 +8,14 @@ export const PhotoDetails = {
 
     ] as string[],
     certificates:[
-        "/photos/certificates/Alegria Technical Head.jpg",
-        "/photos/certificates/Coder of the Semester.jpg",
-        "/photos/certificates/CTFC.jpg",
         "/photos/certificates/Hackoverflow 1.jpg",
         "/photos/certificates/Hackoverflow 2 Participation.jpg",
         "/photos/certificates/Hackoverflow 2 Win.jpg",
-        "/photos/certificates/Kavach 2023.jpg",
+        "/photos/certificates/Alegria Technical Head.jpg",
+        "/photos/certificates/Coder of the Semester.jpg",
         "/photos/certificates/Linear Data Structure Visualization.jpg",
+        "/photos/certificates/CTFC.jpg",
+        "/photos/certificates/Kavach 2023.jpg",
         "/photos/certificates/Math Quiz Win.jpg",
         "/photos/certificates/Alegria Web Dev Javascript.jpg",
         "/photos/certificates/Alegria Automation Python.jpg",
