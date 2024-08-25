07b8059da34879bd4516259d32b84647f7fcb91a
PyPranav <pranavsunil2103@gmail.com>
2024-08-25 09:27:05 +0530

    changed requests to post

--- utils/fetchData.ts
+++ utils/fetchData.ts
@@ -1,17 +1,21 @@
 import { cache } from 'react'
-// const API_ROOT = 'http://127.0.0.1:5000/'
-const API_ROOT = 'https://PyPMac.pythonanywhere.com/'
+const API_ROOT = 'http://127.0.0.1:5000/'
+// const API_ROOT = 'https://PyPMac.pythonanywhere.com/'
 
 
 export const getPlayigSong = async ()=>{
-    const data = await fetch(API_ROOT+'spotify/recently-played').then((res) =>
+    const data = await fetch(API_ROOT+'spotify/recently-played',{
+        method: 'POST'
+    }).then((res) =>
         res.json()
     )
     return data
 }
 
 export const getInstaDetails = cache(async ()=>{
-    const data = await fetch(API_ROOT+'insta').then((res) =>
+    const data = await fetch(API_ROOT+'insta',{
+        method: 'POST'
+    }).then((res) =>
         res.json()
     )
     console.log(data)
@@ -19,7 +23,9 @@ export const getInstaDetails = cache(async ()=>{
 })
 
 export const getGithubDetails = cache(async ()=>{
-    const data = await fetch(API_ROOT+'github').then((res) =>
+    const data = await fetch(API_ROOT+'github',{
+        method: 'POST'
+    }).then((res) =>
         res.json()
     )
     return data
