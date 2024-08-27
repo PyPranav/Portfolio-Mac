b6c991bd7d4cdfbe59300b088f160871557e25f1
PyPranav <pranavsunil2103@gmail.com>
2024-08-27 22:31:40 +0530

    height change mobile

--- components/ios.tsx
+++ components/ios.tsx
@@ -1,6 +1,6 @@
 const IOS = () => {
     return ( 
-    <div className="bg-black text-white text-6xl font-semibold h-screen grid place-items-center">
+    <div className="bg-black h-full text-white text-6xl font-semibold grid place-items-center">
         IOS Work in progress
     </div> 
     );
