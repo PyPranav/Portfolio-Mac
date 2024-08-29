0aecc1defc345359a9a86fd3e4a5b532609bcbee
PyPranav <pranavsunil2103@gmail.com>
2024-08-30 00:00:15 +0530

    supabase to server side

--- components/custom/ArcPages/twitter.tsx
+++ components/custom/ArcPages/twitter.tsx
@@ -1,16 +1,17 @@
 'use client'
-import { createClient } from "@supabase/supabase-js";
+// import { createClient } from "@supabase/supabase-js";
+import { getTweets } from "@/utils/tweetsServer";
 import { useEffect, useState } from "react";
-const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL!,process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!);
-const TwitterPage = () => {
+// const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL!,process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!);
+const TwitterPage =  () => {
     const [tweets, setTweets] = useState<any>([])
     useEffect(()=>{
-        (async ()=>{
-            const { data } = await supabase.from("tweets").select('*').eq('blocked', 'false');
-            console.log({data})
-            setTweets(data);
-        })()
+        getTweets().then((data)=>{
+            console.log(data)
+            setTweets(data)
+        })
     },[])
+
     return ( 
         <section className=" bg-black h-full text-white overflow-y-scroll px-20">
             {tweets?.map((row:any)=>(
