24eafbf9da8646ab80f3bfa5946d0f238eddf17b
PyPranav <pranavsunil2103@gmail.com>
2024-08-30 18:23:14 +0530

    removed avatar component from namewidget

--- components/custom/Widgets/nameWidget.tsx
+++ components/custom/Widgets/nameWidget.tsx
@@ -1,5 +1,6 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { PersonalInfo } from "@/utils/personalInfo";
+import Image from "next/image";
 import { useEffect, useState } from "react";
 
 const NameWidget = ({loaded}:{loaded:boolean}) => {
@@ -14,10 +15,17 @@ const NameWidget = ({loaded}:{loaded:boolean}) => {
     },[ind, loaded])
     return ( 
          <div className='group h-[170px] flex bg-black bg-opacity-10 backdrop-blur-[100px] rounded-2xl p-4 text-white  ' >
-            <Avatar className="w-[139px] h-[139px] opacity-80 group-hover:opacity-100 duration-500">
+            {/* <Avatar className="w-[139px] h-[139px] opacity-80 group-hover:opacity-100 duration-500">
               <AvatarImage src={PersonalInfo.profilePic} />
               <AvatarFallback>CN</AvatarFallback>
-            </Avatar>
+            </Avatar> */}
+            <Image
+                src={PersonalInfo.profilePic}
+                className="object-cover rounded-[130px] opacity-80 group-hover:opacity-100 duration-500"
+                width={139}
+                height={139}
+                alt="Pranav"
+            />
             <div className="px-5 w-full flex flex-col justify-center">
               <p className="text-5xl opacity-80 font-medium group-hover:opacity-100 duration-500 ">
                 {PersonalInfo.name.slice(0,ind)}
