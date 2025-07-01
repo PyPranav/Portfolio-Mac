b0eca5cc6e7e0a38e521a8d770e64c7a6eb47aee
PyPranav <pranavsunil2103@gmail.com>
2025-07-01 20:14:18 +0530

    added outstanding student photos

--- utils/photos.ts
+++ utils/photos.ts
@@ -1,33 +1,32 @@
 export const PhotoDetails = {
-    personal:[
-        '/photos/personal/profile.jpg',
-        '/photos/personal/profile2.jpg',
-        '/photos/personal/hackoverflow.jpg',
-        '/photos/personal/hackoverflow_stage.jpg',
-        '/photos/personal/Technical Secretary.jpeg',
-
-    ] as string[],
-    certificates:[
-        "/photos/certificates/Hackoverflow 1.jpg",
-        "/photos/certificates/Hackoverflow 2 Participation.jpg",
-        "/photos/certificates/Hackoverflow 2 Win.jpg",
-        "/photos/certificates/gccp.png",
-        "/photos/certificates/Alegria Technical Head.jpg",
-        "/photos/certificates/Coder of the Semester.jpg",
-        "/photos/certificates/Linear Data Structure Visualization.jpg",
-        "/photos/certificates/CTFC.jpg",
-        "/photos/certificates/Kavach 2023.jpg",
-        "/photos/certificates/Math Quiz Win.jpg",
-        "/photos/certificates/Alegria Web Dev Javascript.jpg",
-        "/photos/certificates/Alegria Automation Python.jpg",
-
-    ] as string[],
-    projects:[
-        '/photos/projects/gptGoogle1.jpeg',
-        '/photos/projects/gptGoogle2.jpeg',
-        '/photos/projects/toh1.jpeg',
-        '/photos/projects/toh2.jpeg',
-        '/photos/projects/news.png',
-
-    ] as string[]
-}
+  personal: [
+    "/photos/personal/profile.jpg",
+    "/photos/personal/profile2.jpg",
+    "/photos/personal/hackoverflow.jpg",
+    "/photos/personal/hackoverflow_stage.jpg",
+    "/photos/personal/Technical Secretary.jpeg",
+    "/photos/personal/Outstanding Student.jpg",
+  ] as string[],
+  certificates: [
+    "/photos/certificates/Hackoverflow 1.jpg",
+    "/photos/certificates/Hackoverflow 2 Participation.jpg",
+    "/photos/certificates/Hackoverflow 2 Win.jpg",
+    "/photos/certificates/gccp.png",
+    "/photos/certificates/Alegria Technical Head.jpg",
+    "/photos/certificates/Coder of the Semester.jpg",
+    "/photos/certificates/Linear Data Structure Visualization.jpg",
+    "/photos/certificates/CTFC.jpg",
+    "/photos/certificates/Kavach 2023.jpg",
+    "/photos/certificates/Math Quiz Win.jpg",
+    "/photos/certificates/Alegria Web Dev Javascript.jpg",
+    "/photos/certificates/Alegria Automation Python.jpg",
+    "/photos/certificates/Outstanding Student.jpg",
+  ] as string[],
+  projects: [
+    "/photos/projects/gptGoogle1.jpeg",
+    "/photos/projects/gptGoogle2.jpeg",
+    "/photos/projects/toh1.jpeg",
+    "/photos/projects/toh2.jpeg",
+    "/photos/projects/news.png",
+  ] as string[],
+};
